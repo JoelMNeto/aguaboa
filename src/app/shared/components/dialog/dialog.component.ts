2bac942109369b1a0b5d24fb29f170991d4c0c2f
JoelMNeto <joelneto441@gmail.com>
2025-05-07 02:34:15 -0300

    feat: adiciona componente de dialog

---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, Input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'dialog',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss'
+})
+export class DialogComponent {
+
+  readonly dialogRef = inject(MatDialogRef<DialogComponent>);
+
+  data = inject(MAT_DIALOG_DATA);
+
+  onYes() {
+    this.dialogRef.close(true);
+  }
+
+  onNo() {
+    this.dialogRef.close(false);
+  }
+}
