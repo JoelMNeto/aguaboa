4ce0c2f707d42724014153d74b6d269374067fee
JoelMNeto <joelneto441@gmail.com>
2025-05-13 17:56:20 -0300

    fix: arruma formatacao monetaria para tratar valor 0

--- src/app/shared/services/utils.service.ts
+++ src/app/shared/services/utils.service.ts
@@ -8,10 +8,6 @@ export class UtilsService {
   constructor() { }
 
   formataValorMonetario(value: number) {
-    if (value === 0) {
-      return '';
-    }
-
     return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 }
